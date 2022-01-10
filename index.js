36c2b78ae7adbf6abf05d36a2c4f9dc513c8b32f
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-01-10 14:09:12 +0000

    enables class assignment upon init prompt questions

--- index.js
+++ index.js
@@ -7,15 +7,33 @@ const {
   managerQuestions,
 } = require('./src/questions');
 
+const { Engineer, Intern, Manager } = require('./lib');
+
 let employees = [];
 
 const init = async () => {
   const { name, id, email, role } = await inquirer.prompt(employeeQuestions);
   let github;
+  let school;
+  let officeNumber;
+  let obj;
+  let inProgress = true;
   if (role === 'engineer') {
     ({ github } = await inquirer.prompt(engineerQuestions));
+    obj = new Engineer(name, id, email, github);
   }
-  console.log({ name, id, email, role, github });
+  if (role === 'intern') {
+    ({ school } = await inquirer.prompt(internQuestions));
+    obj = new Intern(name, id, email, school);
+  }
+  if (role === 'manager') {
+    ({ officeNumber } = await inquirer.prompt(managerQuestions));
+    obj = new Manager(name, id, email, officeNumber);
+  }
+
+  // console.log({ name, id, email, role, github });
+  console.log(obj);
+  console.log(obj.getRole());
   console.log('Successfully created your employee list!');
 };
 
