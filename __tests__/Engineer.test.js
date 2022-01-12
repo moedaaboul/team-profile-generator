c5aee696154528255dbe4f042f9e512d5096f5f6
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-01-12 01:00:23 +0000

    enables Engineer.test.js functionality which also passes

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,68 @@
+const Engineer = require('../lib/engineer.js');
+
+describe('Engineer', () => {
+  describe('Initialization', () => {
+    it('should create an object with a "name", "id" and "email" if provided valid arguments', () => {
+      const engineer = new Engineer(
+        'Tom',
+        1234,
+        'employee@email.com',
+        'github'
+      );
+      expect(engineer.name).toEqual('Tom');
+      expect(engineer.id).toEqual(1234);
+      expect(engineer.email).toEqual('employee@email.com');
+      expect(engineer.icon).toEqual('fa-glasses');
+    });
+
+    it('should add a method getName()', () => {
+      const engineer = new Engineer(
+        'Tom',
+        1234,
+        'employee@email.com',
+        'github'
+      );
+      expect(engineer.getName()).toEqual('Tom');
+    });
+
+    it('should add a method getId()', () => {
+      const engineer = new Engineer(
+        'Tom',
+        1234,
+        'employee@email.com',
+        'github'
+      );
+      expect(engineer.getId()).toEqual(1234);
+    });
+
+    it('should add a method getEmail()', () => {
+      const engineer = new Engineer(
+        'Tom',
+        1234,
+        'employee@email.com',
+        'github'
+      );
+      expect(engineer.getEmail()).toEqual('employee@email.com');
+    });
+
+    it('should add a method getRole()', () => {
+      const engineer = new Engineer(
+        'Tom',
+        1234,
+        'employee@email.com',
+        'github'
+      );
+      expect(engineer.getRole()).toEqual('Engineer');
+    });
+
+    it('should add a method getIcon()', () => {
+      const engineer = new Engineer(
+        'Tom',
+        1234,
+        'employee@email.com',
+        'github'
+      );
+      expect(engineer.getIcon()).toEqual('fa-glasses');
+    });
+  });
+});
